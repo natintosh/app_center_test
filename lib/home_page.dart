da317e1cfd8880704d3ab1878597a5f0a8c6a5e1
Oluwatobiloba Ogunye <oluwatobi.ogunye@bankly.ng>
2019-09-26 16:48:38 +0100

    _

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,23 +4,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('App Center Test'),
-        centerTitle: true,
-      ),
-      floatingActionButton: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          FloatingActionButton(
-            onPressed: () {},
-            child: Icon(Icons.remove),
-          ),
-          FloatingActionButton(
-            onPressed: () {},
-            child: Icon(Icons.add),
-          ),
-        ],
-      ),
+      appBar: AppBar(title: Text('App Center Test')),
       body: _Content(),
     );
   }
@@ -36,11 +20,40 @@ class _ContentState extends State<_Content> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[Text('Total amount')],
+    return Scaffold(
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                --count;
+              });
+            },
+            child: Icon(Icons.remove),
+          ),
+          SizedBox(
+            height: 4,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                ++count;
+              });
+            },
+            child: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Total count'),
+              Text('$count'),
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
+import 'package:appcenter/appcenter.dart';
+import 'package:appcenter_analytics/appcenter_analytics.dart';
+import 'package:appcenter_crashes/appcenter_crashes.dart';
 import 'package:appcenter_test_app/home_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+
+// f635308d-3e24-4c5c-b0be-f08855ad3dfa ANDROID_GUID
+const String AndroidGUID = 'f635308d-3e24-4c5c-b0be-f08855ad3dfa';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final isIos = defaultTargetPlatform == TargetPlatform.iOS;
+
+    var appSecret = isIos ? '' : AndroidGUID;
+    AppCenter.start(appSecret, [AppCenterCrashes.id, AppCenterAnalytics.id]);
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
