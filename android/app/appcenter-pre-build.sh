2117188834a39447868710e52028354585003607
Oluwatobiloba Ogunye <oluwatobi.ogunye@bankly.ng>
2019-10-14 23:36:17 +0100

    Testing pre build script and appcenter env variables in script

---
+++ android/app/appcenter-pre-build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if ["$APPCENTER_BRANCH" == "master"]; then
+    echo "Master Branch"
+fi
+
+echo "Initializing Flutter Build"
+
+echo "`pwd`"
+
+cd ..
+cd.. 
+
+set -e
+set -x
+
+flutter build apk --release -v
+
+echo "Build Complete"
+
+echo "Moving build apk to release directory"
+
+mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
